72afd84ce4216efe02abb8249e0f894882ad3719
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2018-01-17 10:28:33 +0100

    oversikt_dagens_errors.sh: Lägger till lite extra output för att underlätta användning.

--- shellskript/oversikt_dagens_errors.sh
+++ shellskript/oversikt_dagens_errors.sh
@@ -1,3 +1,13 @@
 TODAY=$(date +%Y.%m.%d)
+echo  
+echo "Söker efter filer vars namn innehåller $TODAY och som innehåller rader som börjar med ERROR:"
+echo  
 grep '^ERROR:' /SASLOG/BatchServer/Logs/*$TODAY*
-more $(grep -l '^ERROR:' /SASLOG/BatchServer/Logs/*$TODAY*)
+if [ $? -ne "0" ]
+then
+	echo "Hurra! Inga fel i dagens loggar!"
+else
+	more $(grep -l '^ERROR:' /SASLOG/BatchServer/Logs/*$TODAY*)
+fi
+
+echo  
